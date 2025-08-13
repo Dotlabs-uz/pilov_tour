a9c5e9d4e06526e5a06c19f404ad018fdd8f3e10
Sardor-web-dev <dzamolovsardor5@gmail.com>
2025-08-13 03:51:59 +0300

    some changes

---
+++ frontend/components/custom/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex justify-between items-center">
+        <div></div>
+        <img src="" alt="" />
+        <div></div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
