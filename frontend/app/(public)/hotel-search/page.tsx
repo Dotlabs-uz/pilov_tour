6df18e4ed0d0651caf18233766c8c59883762303
Sardor-web-dev <dzamolovsardor5@gmail.com>
2025-09-03 17:47:51 +0300

    added:some new pages, fixed:errors with build, will update containers

--- frontend/app/(public)/hotel-search/page.tsx
+++ frontend/app/(public)/hotel-search/page.tsx
@@ -1,9 +1,18 @@
-const HotelSearch = () => {
+import FilterBlock from "@/components/custom/FilterBlock";
+import Subscribe from "@/components/custom/Subcribe";
+import LifeTravel from "@/containers/life-travel";
+import RecTrips from "@/containers/RecTrips";
+import Reviews from "@/containers/Reviews";
+
+export default function HotelSearch() {
   return (
     <>
-      <div></div>
+      <div className="mx-auto flex max-w-[1450px] mt-2 flex-col pr-2 pl-2">
+        <LifeTravel component={<FilterBlock />} />
+        <RecTrips />
+        <Reviews />
+        <Subscribe />
+      </div>
     </>
   );
-};
-
-export default HotelSearch;
+}
---
+++ frontend/app/(public)/stays/page .tsx
@@ -0,0 +1,9 @@
+const Stays = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Stays;
