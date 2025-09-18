900cc17f2a3203e73a3d758687b622bd218731c6
Sardor-web-dev <dzamolovsardor5@gmail.com>
2025-09-19 02:57:58 +0300

    deleted hotel-search page

--- frontend/components/custom/DateRange.tsx
+++ frontend/components/custom/DateRange.tsx
@@ -1,5 +1,5 @@
 import { Input } from "@/components/ui/input";
-import { CiCalendar } from "react-icons/ci";
+// import { CiCalendar } from "react-icons/ci";
 
 export default function DateRange() {
   return (
