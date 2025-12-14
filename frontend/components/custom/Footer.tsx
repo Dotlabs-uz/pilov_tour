e770e4693cbf36341a19f00d474bd42c8fe63607
Danya <danilameretov20@gmail.com>
2025-12-14 20:11:41 +0500

    fixed: Footer

--- frontend/components/custom/Footer.tsx
+++ frontend/components/custom/Footer.tsx
@@ -1,156 +1,141 @@
-"use client";
+"use client"
 
-import { db } from "@/app/(public)/firebase";
-import { Category } from "@/app/(public)/trips/page";
-import { collection, getDocs } from "firebase/firestore";
-import { useTranslations } from "next-intl";
-import { useEffect, useState } from "react";
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaCcVisa,
-  FaCcMastercard,
-} from "react-icons/fa";
+import { db } from "@/app/(public)/firebase"
+import type { Category } from "@/app/(public)/trips/page"
+import { collection, getDocs } from "firebase/firestore"
+import { useTranslations } from "next-intl"
+import { useEffect, useState } from "react"
+import { FaFacebookF, FaInstagram, FaCcVisa, FaCcMastercard } from "react-icons/fa"
 
 const Footer = () => {
-  const t = useTranslations("footer");
-  const [categories, setCategories] = useState<Category[]>([]);
+    const t = useTranslations("footer")
+    const [categories, setCategories] = useState<Category[]>([])
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const snapshot = await getDocs(collection(db, "categories"));
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const snapshot = await getDocs(collection(db, "categories"))
+                const categoryDb: Category[] = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...(doc.data() as Omit<Category, "id">),
+                }))
+                console.log(categoryDb)
+                setCategories(categoryDb)
+            } catch (e) {
+                console.log(e, "Something went wrong")
+            }
+        }
+        fetchCategories()
+    }, [])
 
-        const categoryDb: Category[] = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...(doc.data() as Omit<Category, "id">),
-        }));
+    return (
+        <footer className="bg-[#8DD3BB] pt-28 pb-5 px-6 relative z-0">
+            <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-12 mb-16">
+                    <div>
+                        <p className="text-2xl flex font-bold text-black mb-10">
+                            Pilav{" "}
+                            <span className="text-white">
+                                Tour <u>Agency</u>
+                            </span>
+                        </p>
+                        <div className="flex gap-4 text-2xl text-gray-800">
+                            <a href="#">
+                                <FaFacebookF />
+                            </a>
+                            <a href="#">
+                                <FaInstagram />
+                            </a>
+                        </div>
+                    </div>
 
-        console.log(categoryDb);
-        setCategories(categoryDb);
-      } catch (e) {
-        console.log(e, "Something w+ent wrong");
-      }
-    };
+                    <div>
+                        <h4 className="font-semibold mb-4">{t("destinations")}</h4>
+                        <ul className="space-y-2 text-sm">
+                            {categories.map((category) => (
+                                <li key={category.id}>
+                                    <a href={`/trips?category=${category.destination}`} className="hover:text-black">
+                                        {category.destination}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-    fetchCategories();
-  }, []);
+                    <div className="flex gap-10">
+                        <div>
+                            <h4 className="font-semibold mb-4">{t("company")}</h4>
+                            <ul className="space-y-2 text-sm whitespace-nowrap">
+                                <li>
+                                    <a href="#about" className="hover:text-black">
+                                        {t("about_us")}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-black">
+                                        {t("testimonials")}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-black">
+                                        {t("our_team")}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-black">
+                                        {t("booking_terms")}
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
 
-  return (
-    <footer className="bg-[#8DD3BB] pt-28 pb-5 px-6 relative z-0">
-      <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-12 mb-16">
-          <div>
-            <p className="text-2xl flex font-bold text-black mb-10">
-              Pilav{" "}
-              <span className="text-white">
-                Tour <u>Agency</u>
-              </span>
-            </p>
+                    <div>
+                        <h4 className="font-semibold mb-4">{t("confidentiality")}</h4>
+                        <ul className="space-y-2 text-sm">
+                            <li>
+                                <a href="#" className="hover:text-black">
+                                    {t("cancellations")}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:text-black">
+                                    {t("privacy_policy")}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:text-black">
+                                    {t("sustainability_policy")}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:text-black">
+                                    {t("partnership")}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:text-black">
+                                    {t("contacts")}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
-            <div className="flex gap-4 text-2xl text-gray-800">
-              <a href="#">
-                <FaFacebookF />
-              </a>
-              <a href="#">
-                <FaInstagram />
-              </a>
+                    <div>
+                        <h4 className="font-semibold mb-4">{t("payment_methods")}</h4>
+                        <div className="flex gap-3 text-4xl0">
+                            <FaCcVisa className="text-5xl" color="blue" />
+                            <FaCcMastercard className="text-5xl" color="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="pt-8 border-t border-gray-300 text-center text-sm">
+                    <p>© 2025 Pilav Tour Agency. {t("copyright")}</p>
+                </div>
             </div>
-          </div>
+        </footer>
+    )
+}
 
-          <div>
-            <h4 className="font-semibold mb-4">{t("destinations")}</h4>
-            <ul className="space-y-2 text-sm">
-              {categories.map((category) => (
-                <li key={category.id}>
-                  <a
-                    href={`/trips?=category${category.destination}`}
-                    className="hover:text-black"
-                  >
-                    {category.destination}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="flex gap-10">
-            <div>
-              <h4 className="font-semibold mb-4">{t("company")}</h4>
-              <ul className="space-y-2 text-sm whitespace-nowrap">
-                <li>
-                  <a href="#about" className="hover:text-black">
-                    {t("about_us")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("testimonials")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("our_team")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("booking_terms")}
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <h4 className="font-semibold mb-4">{t("confidentiality")}</h4>
-              <ul className="space-y-2 text-sm whitespace-nowrap">
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("cancellations")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("privacy_policy")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("sustainability_policy")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("partnership")}
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-black">
-                    {t("contacts")}
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <h4 className="font-semibold mb-4 whitespace-nowrap">
-                {t("payment_methods")}
-              </h4>
-              <div className="flex gap-4 items-center">
-                <FaCcVisa className="text-5xl" color="blue" />
-                <FaCcMastercard className="text-5xl" color="" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="text-center text-sm text-gray-800">
-          <p className="font-semibold">{t("company_full_name")}</p>
-          <p className="mt-1">{t("copyright")}</p>
-        </div>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+export default Footer
